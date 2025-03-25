a0a161837aac4cc000795c432e1c24210f9703af
mfurniel <mfurnielc@gmail.com>
2025-03-25 11:43:55 -0300

    chore(swagger): added swagger for documentation

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,19 @@
 import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, { abortOnError: false });
+  app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
+  const config = new DocumentBuilder()
+    .setTitle('Dragon Ball Tournament API')
+    .setDescription('The Dragon Ball Tournament API description')
+    .setVersion('1.0')
+    .addTag('dragon-ball')
+    .build();
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, documentFactory);
   await app.listen(process.env.PORT ?? 3000);
 }
 
