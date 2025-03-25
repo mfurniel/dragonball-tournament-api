8b159f7dfa341ca1302ecc5e7a8b6e3b5dcb3aab
mfurniel <mfurnielc@gmail.com>
2025-03-25 12:10:04 -0300

    chore(config): enable cors in the app

--- src/main.ts
+++ src/main.ts
@@ -14,6 +14,7 @@ async function bootstrap() {
     .build();
   const documentFactory = () => SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, documentFactory);
+  app.enableCors();
   await app.listen(process.env.PORT ?? 3000);
 }
 
