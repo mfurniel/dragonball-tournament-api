092c5b6c0216efceaa26fbcc157e60c022b88bf4
mfurniel <mfurnielc@gmail.com>
2025-03-27 14:12:38 -0300

    test: add unit test for prismaService and prismaModule

---
+++ src/prisma/prisma.module.spec.ts
@@ -0,0 +1,19 @@
+import { Test, TestingModule } from '@nestjs/testing';
+import { PrismaModule } from './prisma.module';
+import { PrismaService } from './prisma.service';
+
+describe('PrismaModule', () => {
+  let prismaService: PrismaService;
+
+  beforeEach(async () => {
+    const module: TestingModule = await Test.createTestingModule({
+      imports: [PrismaModule],
+    }).compile();
+
+    prismaService = module.get<PrismaService>(PrismaService);
+  });
+
+  it('should be defined', () => {
+    expect(prismaService).toBeDefined();
+  });
+});
